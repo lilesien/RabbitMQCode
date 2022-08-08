e3785530186f2184d41ba75280ae05de41f479c3
lilesien <2679467716@qq.com>
2022-08-08 17:14:25 +0800

    完成一些修改操作

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # RabbitMQCode
+
+## 注意事项
+1. 在进行交换机和队列绑定时，可以通过配置类进行声明绑定，也可以通过使用RabbitListener注解进行声明绑定
+2. 对于ttl过期而造成消息变为死信，ttl既可以在声明队列时设置，也可以在发送消息时设置参数
+
+## 死信队列的一些参数
+1. x-message-ttl：指定消息过期TTL
+2. x-dead-letter-exchange：指定队列关联的死信交换机
+3. x-dead-letter-routing-key：指定队列死信交换机绑定的路由键（FanoutExchange不需要设置routingkey）
+
+## 一些routingkey的知识
+1. 在使用routingkey时既可以将值写死，也可以写成一个通配符的样式
+2. 比如写成pohone.*或phone.#，这里的\*代表一个单词，\#代表0个或多个单词
--- comsume/src/main/java/com/lilesien/comsume/DeadMessageQueueListener.java
+++ comsume/src/main/java/com/lilesien/comsume/DeadMessageQueueListener.java
@@ -1,13 +1,18 @@
 package com.lilesien.comsume;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class DeadMessageQueueListener {
 
-    @RabbitListener(bindings = @QueueBinding(
+    //模拟ttl
+/*    @RabbitListener(bindings = @QueueBinding(
             exchange = @Exchange(name = "normal.exchange",type = ExchangeTypes.FANOUT),
             value = @Queue(name = "deadMessage.queue1", arguments = {@Argument(name = "x-dead-letter-exchange", value = "dead.exchange"),
                     @Argument(name = "x-message-ttl",value = "10", type = "java.lang.Integer"),
@@ -16,7 +21,24 @@ public class DeadMessageQueueListener {
     ))
     public void deadMessageListener1(String msg) throws InterruptedException {
         System.out.println("正常队列接收到的消息" + msg);
+    }*/
+
+    //模拟basicNack
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(name = "normal.exchange",type = ExchangeTypes.FANOUT),
+            value = @Queue(name = "deadMessage.queue1", arguments = {@Argument(name = "x-dead-letter-exchange", value = "dead.exchange"),
+                    @Argument(name = "x-message-ttl",value = "10", type = "java.lang.Integer"),
+                    @Argument(name = "x-max-length", value = "3", type = "java.lang.Integer")
+            })
+    ))
+    public void deadMessageListener1(Channel channel, Message message) throws InterruptedException, IOException {
+        System.out.println("正常队列接收到的消息" + new String(message.getBody()));
+        channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,false);
     }
+
+
+
+
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(name = "deadMessage.queue2"),
